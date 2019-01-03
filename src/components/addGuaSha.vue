a85459ce1b28a590b192b10b64517f795dfda9ab
e <2469860777@qq.com>
2019-01-03 16:02:12 +0800

    周四改

--- src/components/addGuaSha.vue
+++ src/components/addGuaSha.vue
@@ -3,31 +3,39 @@
 		<div class="basic">
 			<div>
 				<div>原料名称</div>
-				<div><input type="text" placeholder="请输入" class="yuanlName"></div>
+				<div>
+					<input type="text" placeholder="请输入" class="yuanlName">
+				</div>
 			</div>
 			<div>
 				<div>原料批号</div>
-				<div><input type="text" placeholder="请输入" class="yuanlCode"></div>
+				<div>
+					<input type="text" placeholder="请输入" class="yuanlCode">
+				</div>
 			</div>
 			<div>
 				<div>筒子确认者</div>
 				<div class="rt">
 					<select class="tzConfirmer">
-						<option v-for="item in empList" :value="item.Value">{{item.Text}}</option>
-					</select> <img src="../assets/img/819.png">
+						<option v-for="(item,i) in empList" :value="item.Value" :key="i">{{item.Text}}</option>
+					</select>
+					<img src="../assets/img/819.png">
 				</div>
 			</div>
 			<div>
 				<div>垫圈确认者</div>
 				<div class="rt">
 					<select class="dqConfirmer">
-						<option v-for="item in empList" :value="item.Value">{{item.Text}}</option>
-					</select> <img src="../assets/img/819.png">
+						<option v-for="(item,x) in empList" :value="item.Value" :key="x">{{item.Text}}</option>
+					</select>
+					<img src="../assets/img/819.png">
 				</div>
 			</div>
 			<div style="border: none;">
 				<div>边丝颜色</div>
-				<div><input type="text" placeholder="请输入" class="bsColor"></div>
+				<div>
+					<input type="text" placeholder="请输入" class="bsColor">
+				</div>
 			</div>
 		</div>
 
@@ -48,11 +56,11 @@
 				</div>
 				<div class="block">
 					<span class="demonstration">开始时间</span>
-					<el-date-picker v-model="value1" type="datetime" placeholder="选择日期" class="rt dates startTime" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
+					<el-date-picker v-model="startTime[index]" type="datetime" placeholder="选择日期" class="rt dates" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
 				</div>
 				<div class="block">
 					<span class="demonstration">结束时间</span>
-					<el-date-picker v-model="value2" type="datetime" placeholder="选择日期" class="rt dates endTime" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
+					<el-date-picker v-model="endTime[index]" type="datetime" placeholder="选择日期" class="rt dates" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
 				</div>
 				<div style="border-bottom: none;">
 					<div>班别</div>
@@ -65,18 +73,18 @@
 			</div>
 		</div>
 
-		<div class="addNew" @click="addNew">
-			<img src="../assets/img/3893.png" />
-		</div>
-		<div class="posit" @click="confirms">
-			<p>确定</p>
-		</div>
+	<div class="addNew" @click="addNew">
+		<img src="../assets/img/3893.png">
+	</div>
+	<div class="posit" @click="confirms">
+		<p>确定</p>
+	</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'applydetail',
+		name: "applydetail",
 		data() {
 			return {
 				num: 1,
@@ -87,113 +95,126 @@
 						return time.getTime() > Date.now();
 					}
 				},
-				value1:'',
-				value2:'',
-				itemRadios:['value1','value2','value3','value4','value5','value6','value7','value8','value9','value10']
-			}
+				startTime: [''],
+				endTime: ['']
+			};
 		},
 		methods: {
 			addNew: function() {
-				this.num++
+				this.num++;
 			},
 			confirms: function() {
 				//非空验证
-				if($('.yuanlName').val() == "" || $('.yuanlCode').val() == "" || $('.tzConfirmer').val() == "" || $('.dqConfirmer').val() == "" || 
-					$('.bsColor').val() == "" || $('.zeren').val() == "" || $('.tongzNum').val() == "" || this.value1 == "" ||  this.value2 == "" ||$('.classBan').val() == "") {
-					
+				if(
+					$(".yuanlName").val() == "" ||$(".yuanlCode").val() == "" ||$(".tzConfirmer").val() == "" ||$(".dqConfirmer").val() == "" ||
+					$(".bsColor").val() == "" ||$(".zeren").val() == "" ||$(".tongzNum").val() == "" || $('.el-input__inner').val() == "" ||$(".classBan").val() == ""){
 					this.$message({
-			          showClose: true,
-			          message: '请完善信息',
-			          type: 'error',
-			          center: true
-			        });
+						showClose: true,
+						message: "请完善信息",
+						type: "error",
+						center: true
+					});
+					for(var i=0;i<$('input.el-input__inner').length;i++){
+						console.log($('input.el-input__inner').val())
+						if($('input.el-input__inner').val() == ""){
+							this.$message({
+								showClose: true,
+								message: "请完善信息",
+								type: "error",
+								center: true
+							});
+						}
+					}
+					
 				} else {
-				//不为空之后
-				console.log()
+					//不为空之后
+					console.log();
 					let entity = {};
-					entity.MName = $('.yuanlName').val();
-					entity.BatchNo = $('.yuanlCode').val();
-					entity.CheeseNumChekerID = $('.tzConfirmer').val();
-					entity.CheeseNumChekerName = $('.tzConfirmer option:selected').text();
-					entity.CloutCheckerID = $('.dqConfirmer').val();
-					entity.CloutCheckerName = $('.dqConfirmer option:selected').text();
-					entity.EdgWireColor = $('.bsColor').val();
+					entity.MName = $(".yuanlName").val();
+					entity.BatchNo = $(".yuanlCode").val();
+					entity.CheeseNumChekerID = $(".tzConfirmer").val();
+					entity.CheeseNumChekerName = $(".tzConfirmer option:selected").text();
+					entity.CloutCheckerID = $(".dqConfirmer").val();
+					entity.CloutCheckerName = $(".dqConfirmer option:selected").text();
+					entity.EdgWireColor = $(".bsColor").val(); 
 
 					let emplist = [];
 
-					for(let i = 0; i < $('.add-item').length; i++) {
+					for(let i = 0; i < $(".add-item").length; i++) {
 						let emplisters = {};
 
-						emplisters.EmpID = $('.zeren').eq(i).val();
-						emplisters.EmpName = $('.zeren option:selected').eq(i).text();
-						emplisters.DoNum = $('.tongzNum').eq(i).val();
-						emplisters.BeginTime = this.value1;
-						emplisters.EndTime = this.value2;
-						emplisters.ClassBan = $('.classBan').eq(i).val();
-						emplisters.ClassBanName = $('.classBan option:selected').eq(i).text();
-
-						emplist.push(emplisters)
+						emplisters.EmpID = $(".zeren").eq(i).val();
+						emplisters.EmpName = $(".zeren option:selected").eq(i).text();
+						emplisters.DoNum = $(".tongzNum").eq(i).val();
+						emplisters.BeginTime = this.startTime[i];
+						emplisters.EndTime = this.endTime[i];
+						emplisters.ClassBan = $(".classBan").eq(i).val();
+						emplisters.ClassBanName = $(".classBan option:selected").eq(i).text();
+						emplist.push(emplisters);
 					}
 					this.$axios({
-						method: 'post',
-						url: 'api/WarpingOrder/SaveWarYarnHung',
-						data: {
-							entity: entity,
-							empjson: emplist
-						}
-					}).then((res) => {
-						console.log(res);
-					}).catch((error) => {
-						console.log(error);
-					});
-
+							method: "post",
+							url: "api/WarpingOrder/SaveWarYarnHung",
+							data: {
+								entity: entity,
+								empjson: emplist
+							}
+						})
+						.then(res => {
+							console.log(res);
+						})
+						.catch(error => {
+							console.log(error);
+						});
 				}
-
 			},
 			tzConfirmerList: function() {
 				//筒子确认者列表
 				this.$axios({
-					method: 'post',
-					url: 'api/WarpingOrder/GetEmpDropDownList',
-				}).then((res) => {
-					console.log(res);
-					this.empList = res.data.data;
-				}).catch((error) => {
-					console.log(error);
-				});
+						method: "post",
+						url: "api/WarpingOrder/GetEmpDropDownList"
+					})
+					.then(res => {
+						console.log(res);
+						this.empList = res.data.data;
+					})
+					.catch(error => {
+						console.log(error);
+					});
 				//垫圈确认者列表
 				this.$axios({
-					method: 'post',
-					url: 'api/WarpingOrder/GetBShiftDrpDownList',
-				}).then((res) => {
-					console.log(res);
-					this.drpList = res.data.data;
-				}).catch((error) => {
-					console.log(error);
-				});
-
+						method: "post",
+						url: "api/WarpingOrder/GetBShiftDrpDownList"
+					})
+					.then(res => {
+						console.log(res);
+						this.drpList = res.data.data;
+					})
+					.catch(error => {
+						console.log(error);
+					});
 			}
 		},
 		created() {
-			this.tzConfirmerList()
+			this.tzConfirmerList();
 		}
-	}
+	};
 </script>
 
 <style scoped lang="less">
 	.bg1 {
 		position: relative;
-		font-size: .17rem;
+		font-size: 0.17rem;
 		height: auto;
 		min-height: 6.7rem;
-		padding-bottom: .7rem;
+		padding-bottom: 0.7rem;
 		.basic {
 			background-color: white;
-			line-height: .5rem;
-			padding-left: .15rem;
+			line-height: 0.5rem;
+			padding-left: 0.15rem;
 			>div {
-				height: .5rem;
-				border-bottom: 1px solid #D5D5D5;
+				height: 0.5rem;
+				border-bottom: 1px solid #d5d5d5;
 			}
 			>div div {
 				display: inline-block;
@@ -205,13 +226,13 @@
 			}
 			input {
 				width: 2.2rem;
-				height: .35rem;
-				line-height: .35rem;
-				padding-left: .07rem;
-				font-size: .17rem;
+				height: 0.35rem;
+				line-height: 0.35rem;
+				padding-left: 0.07rem;
+				font-size: 0.17rem;
 				border: none;
 				outline: none;
-				background-color: #F3F3F3;
+				background-color: #f3f3f3;
 				color: #999;
 			}
 			select {
@@ -222,16 +243,16 @@
 				appearance: none;
 				-moz-appearance: none;
 				-webkit-appearance: none;
-				text-indent: .02rem;
+				text-indent: 0.02rem;
 				background-color: transparent;
-				font-size: .17rem;
+				font-size: 0.17rem;
 				color: #999;
-				width: .8rem;
+				width: 0.8rem;
 			}
 			img {
-				margin-right: .15rem;
-				width: .09rem;
-				height: .17rem;
+				margin-right: 0.15rem;
+				width: 0.09rem;
+				height: 0.17rem;
 			}
 		}
 	}
@@ -241,41 +262,45 @@
 		bottom: 0;
 		left: 0;
 		right: 0;
-		height: .5rem;
-		line-height: .5rem;
+		height: 0.5rem;
+		line-height: 0.5rem;
 		text-align: center;
-		background-color: #007EFF;
+		background-color: #007eff;
 		color: white;
 	}
 	
 	.addNew {
 		position: fixed;
-		bottom: .65rem;
-		left: .97rem;
+		bottom: 0.65rem;
+		left: 0.97rem;
 		img {
 			width: 1.65rem;
 		}
 	}
 	
 	.add-item {
-		width: 3.4rem!important;
+		width: 3.4rem !important;
 		margin: 0 auto;
 		p {
-			margin: .15rem 0 .1rem .05rem;
+			margin: 0.15rem 0 0.1rem 0.05rem;
 			color: #999999;
-			font-size: .15rem;
+			font-size: 0.15rem;
 		}
 		input {
-			width: 2.1rem!important;
-			margin-right: .15rem;
+			width: 2.1rem !important;
+			margin-right: 0.15rem;
 		}
 		#tongNum {
-			width: .9rem;
+			width: 0.9rem;
 		}
 	}
 	
+	.add-item:last-child {
+		margin-bottom: 0.5rem!important;
+	}
+	
 	.dates {
-		margin-right: .15rem;
+		margin-right: 0.15rem;
 		width: 2.16rem;
 	}
 </style>
